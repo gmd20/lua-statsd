2b0e9ef2cc2d6713cb3dce49227e42f881891eb6
gmd20 <gmd20@163.com>
2014-06-13 11:45:59 +0800

    fix bug in clock_gettime

--- linux_optimization_version/quick_sort/quick_sort.cpp
+++ linux_optimization_version/quick_sort/quick_sort.cpp
@@ -16,10 +16,15 @@ extern "C"  {
 
   double gettime()
   {
+    /* 注意只有CLOCK_REALTIME返回的是从“00:00 hours, Jan 1, 1970 UTC” */
+    /* 开始的秒数，才能用于那个graphite里面timestamp，其他的都是相对时间， */
+    /* CLOCK_MONOTONIC_COARSE这个说事last tick时间，但粒度太粗了。 */
+    /* 但我的ubuntu虚机上面 clock_gettime 比gettimeofday慢好多，可能是时钟源的问题。 */
     /* struct timespec tp; */
+    /* clock_gettime(CLOCK_REALTIME, &tp); */
     /* clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tp); */
-    /* /1* clock_gettime(CLOCK_MONOTONIC_COARSE, &tp); *1/ */
-    /* double now = tp.tv_sec + ((double)tp.tv_nsec ) /1000000; */
+    /* clock_gettime(CLOCK_MONOTONIC_COARSE, &tp); **/
+    /* double now = tp.tv_sec + ((double)tp.tv_nsec ) /1.0e9; */
     /* return now; */
 
     struct timeval v;
